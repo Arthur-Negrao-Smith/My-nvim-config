0f4c4f487026f440fc7e48a27e83661a0cd8abe7
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-04-16 15:47:13 -0300

    Add keymaps to file manipulations and tree manipulation

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -1,5 +1,16 @@
+local keyset = vim.keymap.set
+
 -- Change split window position
-vim.keymap.set('n', '<C-S-h>', '<C-w>H', { desc = 'Move window to the left' })
-vim.keymap.set('n', '<C-S-l>', '<C-w>L', { desc = 'Move window to the right' })
-vim.keymap.set('n', '<C-S-j>', '<C-w>J', { desc = 'Move window to the lower' })
-vim.keymap.set('n', '<C-S-k>', '<C-w>K', { desc = 'Move window to the upper' })
+keyset('n', '<C-S-h>', '<C-w>H', { desc = 'Move window to the left' })
+keyset('n', '<C-S-l>', '<C-w>L', { desc = 'Move window to the right' })
+keyset('n', '<C-S-j>', '<C-w>J', { desc = 'Move window to the lower' })
+keyset('n', '<C-S-k>', '<C-w>K', { desc = 'Move window to the upper' })
+
+-- Neo-tree keybinds
+keyset('n', '<leader>er', '<Cmd>Neotree reveal<CR>', { desc = 'Reveal current file on Neotree' })
+
+-- File manipulation
+keyset({ 'n', 'v', 'i' }, '<leader>ww', '<Cmd>:w<CR>', { desc = 'Save file' })
+keyset({ 'n', 'v', 'i' }, '<leader>wq', '<Cmd>:q<CR>', { desc = 'Quit file' })
+keyset({ 'n', 'v', 'i' }, '<leader>wc', '<Cmd>:qa!<CR>', { desc = 'Force quit file' })
+keyset({ 'n', 'v', 'i' }, '<leader>ws', '<Cmd>:wq<CR>', { desc = 'Save and quit file' })
