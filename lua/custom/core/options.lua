0fe72e1834070c70bc01fa5688dee2ff05f4ce98
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-04-16 13:12:32 -0300

    Add dir custom/core

---
+++ lua/custom/core/options.lua
@@ -0,0 +1,17 @@
+-- line numbers
+vim.opt.relativenumber = true
+vim.opt.shell = '/bin/zsh'
+vim.opt.number = true
+
+-- tabs & indents
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+
+-- Set wrap line
+vim.opt.wrap = true
+
+-- Splite windows
+vim.opt.splitright = true
+vim.opt.splitbelow = true
